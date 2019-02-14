99b480b8cf1a83bb6bbc5dab3882110658808ac5
Dan K <kuchandw@gmail.com>
2019-02-13 21:35:50 -0800

    Update Starry_Night_Board_Checkout.cpp
    A more unified concept of operation I think.

--- Starry_Night_Board_Checkout.cpp
+++ Starry_Night_Board_Checkout.cpp
@@ -4,6 +4,7 @@
 
 byte lighting_command;
 int pwm_register_set[]={B0001000, B0001001, B0001010,}
+
 void setup()
 {
   Serial.begin(115200);
@@ -21,28 +22,39 @@ void loop()
     send_data();
     delay(100);
 
-    for(int i; i<=16, i++){
-        high_side_command << 1;
-        send_data(high_side_command, low_side_command);
-        delay(100);
-    }
+for (int i, i<16, i++){
+  high_side_command << 1;
+  send_data(0, high_side_command);
+  delay(100);
+  for(int j; j<=16, j++){
+      send_data(1, build_command_set(j, 1, 255));
+
+  }
+
 }
 
-void send_data(uint_16t cmd1, uint_16t cmd2) {
+}
+
+void send_data(int target, int cmd) {
     //This sends both high side and low side data
   Serial.println("Entered Send Data : "); //Send Serial Flag
-  Serial.print("High Side Command: ");
-  Serial.print(cmd1);
-  Serial.print("   Low Side Command: ");
-  Serial.print(cmd2);
+  Serial.print("Command: ");
+  Serial.print(cmd);
   Serial.println();
-  digitalWrite(ss1, LOW);
+  if (target ==0){}
+    digitalWrite(ss1, LOW);
+  }
+  else{
+    digitalWrite(ss2, LOW)
+  }
   SPI.beginTransaction(SPISettings(5000000, MSBFIRST, SPI_MODE0));  //3 Mhz should be more than fast enough for this application.
-  SPI.transfer(cmd1);
-  digitalWrite(ss1, HIGH);
-  digitalWrite(ss2, LOW);
-  SPI.transfer(cmd2);
-  digitalWrite(ss2, HIGH);
+  SPI.transfer(cmd);
+  if (target ==0){}
+    digitalWrite(ss1, HIGH);
+  }
+  else{
+    digitalWrite(ss2, HIGH)
+  }
   SPI.endTransaction();
 }
 
@@ -52,11 +64,11 @@ int build_command_set(int lednum, int readwrite, int command_val){
     int typebit;
     if (readwrite ==1){
         typebit = B0000000100000000;
-    }  
+    }
     else {
 
         typebit = 0;
-    }  
+    }
     regset = pwm_register_set[lednum];
     regset << 1;
     regset = regset | typebit;
